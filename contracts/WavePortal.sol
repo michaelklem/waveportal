6986cfbfb3d5c90e6e39ec94ee7f116b972c544b
Michael Klem <michael.klem@gmail.com>
2021-11-18 14:44:15 -0800

    Completed

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -9,16 +9,55 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract WavePortal is Ownable{
   uint MAX_WAVES_ALLOWED = 10;
 
+  event NewWave(address indexed from, uint256 timestamp, string message);
+  struct Wave {
+      address waver; // The address of the user who waved.
+      string message; // The message the user sent.
+      uint256 timestamp; // The timestamp when the user waved.
+  }
+
+  /*
+    * I declare a variable waves that lets me store an array of structs.
+    * This is what lets me hold all the waves anyone ever sends to me!
+    */
+  Wave[] waves;
+
   // This stores to total number of waves sent to this contract.
   uint256 totalWaves;
 
   // This stored the number of waves made by specific addresses.
   mapping (address => uint) totalWaveByAddress;
 
-  constructor() {
-      console.log("Hi Farza!!!");
+  /*
+    * This is an address => uint mapping, meaning I can associate an address with a number!
+    * In this case, I'll be storing the address with the last time the user waved at us.
+    */
+  mapping(address => uint256) public lastWavedAt;
+
+
+
+  /*
+    * We will be using this below to help generate a random number
+    */
+  uint256 private seed;
+
+  constructor() payable {
+    console.log("Contructed!!!");
+    /*
+    * Set the initial seed
+    */
+    seed = getRando();
   }
 
+  /*
+  * Generate a new seed for the next user that sends a wave
+  */
+  function getRando() internal view returns (uint256) {
+    uint256 tempSeed = (block.timestamp + block.difficulty) % 100;
+    console.log("Random number generated: %d", tempSeed);
+    return tempSeed;
+  }
+  
   modifier notBotheringMeYet {
     // We ignore this logic for the owner of the contract so they can wave as many times as needed.
     if (! isOwner()) {
@@ -32,14 +71,60 @@ contract WavePortal is Ownable{
     return msg.sender == owner();
   }
 
-  function wave() public notBotheringMeYet {
+  function wave(string memory _message) public notBotheringMeYet {
+    /*
+      * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+      */
+    require(
+        lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+        "Wait 30 seconds before you can wave at me again."
+    );
+
+
+    /*
+      * Update the current timestamp we have for the user
+      */
+    lastWavedAt[msg.sender] = block.timestamp;
+
+
     totalWaves += 1;
 
     // Update the total waves sent by address
     uint tempWaves = totalWaveByAddress[msg.sender];
     totalWaveByAddress[msg.sender] = ++tempWaves;
-    
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    seed = getRando();
+    /*
+      * Give a 50% chance that the user wins the prize.
+      */
+    if (seed <= 50) {
+        console.log("%s won!", msg.sender);
+
+        /*
+        * The same code we had before to send the prize.
+        */
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+    }
+
+    emit NewWave(msg.sender, block.timestamp, _message);
+
     console.log("%s has waved!", msg.sender);
+  }
+
+  /*
+    * I added a function getAllWaves which will return the struct array, waves, to us.
+    * This will make it easy to retrieve the waves from our website!
+    */
+  function getAllWaves() public view returns (Wave[] memory) {
+      return waves;
   }
 
   function getTotalWaves() public view returns (uint256) {
--- scripts/run.js
+++ scripts/run.js
@@ -2,38 +2,43 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
+  /*
+   * Get Contract balance
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
   let waveCount;
   let waveTxn;
 
   waveCount = await waveContract.getTotalWaves();
-  
+  console.log('Total waves at start: ' + waveCount.toNumber());
+
   try {
     // the owner can wave as many times as they want
     for (let i = 0; i < 15; i++) {
-      waveTxn = await waveContract.wave();
+      waveTxn = await waveContract.wave('my message');
       await waveTxn.wait();
     }
 
     // the owner can wave as many times as they want
     for (let i = 0; i < 13; i++) {
-      waveTxn = await waveContract.connect(randomPerson).wave();
+      waveTxn = await waveContract.connect(randomPerson).wave('some other message');
       await waveTxn.wait();
     }
-
-    // waveTxn = await waveContract.connect(randomPerson).wave();
-    // await waveTxn.wait();
-
-    // waveTxn = await waveContract.connect(randomPerson).wave();
-    // await waveTxn.wait();
-
-    // waveTxn = await waveContract.connect(randomPerson).wave();
-    // await waveTxn.wait();
   }
   catch(err) {
     console.log('Error: ' + err.message)
@@ -46,6 +51,18 @@ const main = async () => {
   console.log('Number of time the rando waved: ' + randoWaves)
   
   waveCount = await waveContract.getTotalWaves();
+  console.log('Total waves at end: ' + waveCount.toNumber());
+
+
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    'Final Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
 };
 
 const runMain = async () => {
